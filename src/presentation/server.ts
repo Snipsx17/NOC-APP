175e275383375545a62c86dd6b6e951e87914d80
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-11-08 10:21:26 -0500

    Feat: Create server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,12 @@
+import { CronJob } from "cron";
+import { CronService } from "./services.cron-service";
+import { CheckService } from "../domain/use-cases/check-sevice.use-case";
+
 export class Server {
   public static start() {
     console.log("Server started...");
+    CronService.createService("*/3 * * * * *", () => {
+      new CheckService().execute("http://localhost:3000/posts");
+    });
   }
 }
