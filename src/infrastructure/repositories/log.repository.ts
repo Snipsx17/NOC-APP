1377df8afb0b945f373151a89f08e1889684aa69
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-11-10 22:04:38 -0500

    Feat: Add log repository

---
+++ src/infrastructure/repositories/log.repository.ts
@@ -0,0 +1,15 @@
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log-entity";
+import { LogDataRepository } from "../../domain/respository/log.respository";
+
+export class LogRepositoryImpl implements LogDataRepository {
+  constructor(private readonly logDataSource: LogDataSource) {}
+
+  saveLog(log: LogEntity): Promise<void> {
+    return this.logDataSource.saveLog(log);
+  }
+
+  getLog(severity: LogSeverityLevel): Promise<LogEntity[]> {
+    return this.logDataSource.getLog(severity);
+  }
+}
