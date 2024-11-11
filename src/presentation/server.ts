081b12593f8165935e590c5338fddd3df15071d3
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-11-10 22:06:55 -0500

    Feat: implements FileSystemRepository in server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,12 @@
 import { CronJob } from "cron";
 import { CronService } from "./services.cron-service";
 import { CheckService } from "../domain/use-cases/check-sevice.use-case";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+
+const FileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource(),
+);
 
 export class Server {
   public static start() {
@@ -8,6 +14,7 @@ export class Server {
     console.log("Server started...");
     CronService.createService("*/3 * * * * *", () => {
       new CheckService(
+        FileSystemLogRepository,
         () => console.log("\x1b[42m", `Service: ${url} IS UP.`),
         () => console.error("\x1b[41m", `Service ${url} IS DOWN.`),
       ).execute(url);
