25d96d021906b13a3979764943fb982c9aac329a
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-11-15 14:07:59 -0500

    Feat: Implement email service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,8 @@ import { CronService } from "./services/cron-service";
 import { CheckService } from "@/domain/use-cases/check-sevice.use-case";
 import { LogRepositoryImpl } from "@/infrastructure/repositories/log.repository";
 import { FileSystemDataSource } from "@/infrastructure/datasources/file-system.datasource";
+import { envs } from "@/config/plugins/envs.plugin";
+import { EmailService } from "./services/email.service";
 
 const FileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDataSource(),
@@ -12,13 +14,22 @@ export class Server {
     console.log("Server started...");
 
     const url = "http://uhernandez.com";
-
-    CronService.createService("*/3 * * * * *", () => {
-      new CheckService(
-        FileSystemLogRepository,
-        () => console.log("\x1b[42m", `Service: ${url} IS UP.`),
-        () => console.error("\x1b[41m", `Service ${url} IS DOWN.`),
-      ).execute(url);
+    new EmailService().send({
+      to: "snipsx0912@gmail.com",
+      subject: "test email",
+      HTMLbody: `
+      <h1>Service status</h1>
+      <br />
+      <p>Commodo aliquip ipsum laborum sit dolore ad ea dolor proident velit et aliquip. Dolore eiusmod aliquip adipisicing in quis laborum sit. Duis magna nisi cillum nisi aliqua officia adipisicing sint non Lorem quis excepteur commodo.</p>
+    `,
     });
+
+    // CronService.createService("*/3 * * * * *", () => {
+    //   new CheckService(
+    //     FileSystemLogRepository,
+    //     () => console.log("\x1b[42m", `Service: ${url} IS UP.`),
+    //     () => console.error("\x1b[41m", `Service ${url} IS DOWN.`),
+    //   ).execute(url);
+    // });
   }
 }
