8b302af20fcdb28cc2280e4141a56f94902141ac
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-11-18 20:16:11 -0500

    Feat: Implements mongo DB connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
 import { Server } from "@/presentation/server";
+import { MongoDB } from "./data/mongo/init";
+import { envs } from "./config/plugins/envs.plugin";
 
-const main = (): void => {
+const main = async (): Promise<void> => {
+  try {
+    await MongoDB.connect({
+      dbUri: envs.MONGO_URL,
+      dbName: envs.MONGO_DB_NAME,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
   Server.start();
 };
 
 (async () => {
-  main();
+  await main();
 })();
