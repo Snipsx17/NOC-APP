08bd5c99ee25cf3a3057659da67357b7da22cf50
Snipsx17 <ing.uberthhernandez@gmail.com>
2024-11-18 20:12:20 -0500

    Feat: add mongo envs

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -7,4 +7,10 @@ export const envs = {
   MAILER_ADDRESS: env.get("MAILER_ADDRESS").required().asEmailString(),
   MAILER_SECRET: env.get("MAILER_SECRET").required().asString(),
   PROD: env.get("PROD").required().asString(),
+
+  //MONGO DB
+  MONGO_URL: env.get("MONGO_URL").required().asString(),
+  MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
+  MONGO_USER: env.get("MONGO_USER").required().asString(),
+  MONGO_PASS: env.get("MONGO_PASS").required().asString(),
 };
